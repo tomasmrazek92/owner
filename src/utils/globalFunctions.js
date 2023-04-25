3557e0799fa220773a4931ea32509245562a0ae3
tomasmrazek92 <tomasmrazek92@gmail.com>
2023-04-25 13:40:11 +0200

    Init

--- src/utils/globalFunctions.js
+++ src/utils/globalFunctions.js
@@ -1,46 +1,83 @@
-export const swipers = [];
+const getPlaceFromSessionStorage = () => {
+  const value = localStorage.getItem("restaurant");
+  return value ? JSON.parse(value) : null;
+};
 
-export const createSwiper = (componentSelector, sliderSelector, classSelector, options) => {
-  // Globals
-  const arrows = '.slider-arrow';
-  const pagination = '.swiper-navigation';
-  // For Each
-  $(componentSelector).each(function () {
-    // Tag Instance
-    let index = $(this).index();
-    let instanceClass = `${classSelector}_${index}`;
-    $(this).find(sliderSelector).addClass(instanceClass);
-    $(this).find(arrows).addClass(instanceClass);
-    $(this).find(pagination).addClass(instanceClass);
+const setGooglePlaceDataToForm = (googlePlace) => {
+  if (!googlePlace) return;
 
-    // Build Options
-    let swiperOptions = Object.assign({}, options, {
-      speed: 250,
-      navigation: {
-        prevEl: `${arrows}.prev.${instanceClass}`,
-        nextEl: `${arrows}.next.${instanceClass}`,
-      },
-      pagination: {
-        el: `${pagination}.${instanceClass}`,
-        type: 'bullets',
-        bulletActiveClass: 'w-active',
-        bulletClass: 'w-slider-dot',
-      },
+  const componentForm = {
+    name: "",
+    international_phone_number: "",
+    website: "",
+    place_id: "",
+    rating: "",
+    user_ratings_total: "",
+    address_components: {
+      street_number: "short_name",
+      route: "long_name",
+      locality: "long_name",
+      administrative_area_level_1: "short_name",
+      country: "short_name",
+      postal_code: "short_name",
+    },
+  };
+
+  if (googlePlace.address_components) {
+    let route = "";
+    let streetNumber = "";
+
+    googlePlace.address_components.forEach(component => {
+      const addressType = component.types[0];
+      const type = componentForm.address_components[addressType];
+      const input = $(`input[name=${addressType}]`);
+
+      if (type) {
+        const val = component[type];
+        if (addressType === "route") route = val;
+        else if (addressType === "street_number") streetNumber = val;
+        else input.val(val);
+      }
     });
 
-    // Update Options
-    for (let key in options) {
-      if (key in swiperOptions) {
-        swiperOptions[key] = options[key];
-      }
-    }
+    $("input[name=restaurant-address]").val(`${streetNumber} ${route}`);
+  }
 
-    // Init Slider
-    let swiper = new Swiper(`${sliderSelector}.${instanceClass}`, swiperOptions);
+  if (googlePlace.types) {
+    const typesAsString = googlePlace.types.join(", ");
+    $("input[name=place_types]").val(typesAsString);
+  }
 
-    // Push to Global for possible references
-    // store swiper instance in object using classSelector as key
-    swipers[classSelector] = swipers[classSelector] || {};
-    swipers[classSelector][index] = swiper;
+  Object.keys(componentForm).forEach(key => {
+    if (key === "address_components") return;
+    const value = googlePlace[key];
+    const input = $(`input[name=${key}]`);
+    if (input) input.val(value);
   });
 };
+
+const googlePlaceFromStorage = getPlaceFromSessionStorage();
+if (googlePlaceFromStorage) {
+  setGooglePlaceDataToForm(googlePlaceFromStorage);
+  $('input[name="restaurant-name"]').val(localStorage.getItem("restaurant-value"));
+}
+
+const gpaOptions = {
+  componentRestrictions: {country: "us"}
+};
+
+$('input[name="restaurant-name"]').each(function () {
+  const autocomplete = new google.maps.places.Autocomplete(this, gpaOptions);
+  const self = $(this);
+
+  autocomplete.addListener("place_changed", function () {
+    const place = autocomplete.getPlace();
+    const value = self.val();
+
+    setGooglePlaceDataToForm(place);
+    localStorage.setItem("restaurant-value", value);
+    localStorage.setItem("restaurant", JSON.stringify(place));
+    $('input[name="restaurant-name"]').val(localStorage.getItem("restaurant-value"));
+    $('input[name="restaurant-name"]').siblings(".field-validation").hide();
+  });
+});
---
+++ src/utils/localStorage.js
@@ -0,0 +1,8 @@
+export const getItem = (key) => {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  };
+  
+  export const setItem = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
