b72ed4fa1c2c050fccb4053628a3f44419a09705
tomasmrazek92 <tomasmrazek92@gmail.com>
2023-06-14 13:38:05 +0200

    Homepage Swiper Fix 2

--- src/homepage.js
+++ src/homepage.js
@@ -70,6 +70,9 @@ $(document).ready(() => {
           });
 
           isInView = true;
+
+          // Disconnect the observer after the first intersection
+          observer.disconnect();
         }
       }
     });
