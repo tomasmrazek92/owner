63e44de8069bc66e14ff87764ccb51c7454c39b5
tomasmrazek92 <tomasmrazek92@gmail.com>
2023-06-16 01:17:34 +0200

    Small Updates

--- src/blog.js
+++ src/blog.js
@@ -1,7 +1,7 @@
 import { createSwiper } from '$utils/swipers';
 
 // Swipers
-let blogSwipers = $('.slider.n_blog-post');
+let blogSwipers = $('.slider.n_blog-post').not('#swiper-related');
 if (blogSwipers) {
   createSwiper($(blogSwipers), '.swiper.n_blog-post', 'blog-swiper', {
     spaceBetween: 20,
@@ -29,6 +29,50 @@ if (blogSwipers) {
   });
 }
 
+let blogDetail = $('.n_section-blog_other');
+
+if (blogDetail) {
+  let swiper;
+  let init = false;
+
+  function swiperMode() {
+    const mobile = window.matchMedia('(min-width: 0px) and (max-width: 991px)');
+    const desktop = window.matchMedia('(min-width: 992px)');
+
+    // Disable (for desktop)
+    if (desktop.matches) {
+      if (init) {
+        swiper.destroy(true, true);
+        init = false;
+      }
+    }
+
+    // Enable (for Mobile)
+    else if (mobile.matches) {
+      if (!init) {
+        init = true;
+        swiper = new Swiper('.swiper.n_blog-post', {
+          slidesPerView: 1.1,
+          spaceBetween: 20,
+          navigation: {
+            prevEl: `.swiper_arrow.n_blog-post.prev`,
+            nextEl: `.swiper_arrow.n_blog-post.next`,
+          },
+        });
+      }
+    }
+  }
+
+  // Load
+  window.addEventListener('load', function () {
+    swiperMode();
+  });
+
+  // Resize
+  window.addEventListener('resize', function () {
+    swiperMode();
+  });
+}
 // Filters
 $('.n_blog-radio-field').on('click', function () {
   if ($(this).is('[fs-cmsfilter-element="clear"]')) {
