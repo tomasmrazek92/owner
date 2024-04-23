9a6eebbb9386d59fca77fc3d38a5c4b143d8dadc
Chris Smearing <chris.smearing@siegemedia.com>
2024-04-23 15:16:58 -0500

    Add profit margin calculator
    Added script and style for calculator;
    Added sass package and package script to build styles;

--- bin/build.js
+++ bin/build.js
@@ -8,15 +8,16 @@ const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
 const ENTRY_POINTS = [
-  'src/index.js',
-  'src/webgenLead.js',
-  'src/homepage.js',
-  'src/demo.js',
-  'src/blog.js',
-  'src/careers.js',
-  'src/story.js',
-  'src/lottie.js',
-  'src/demo-test.js',
+    'src/index.js',
+    'src/webgenLead.js',
+    'src/homepage.js',
+    'src/demo.js',
+    'src/blog.js',
+    'src/careers.js',
+    'src/story.js',
+    'src/lottie.js',
+    'src/demo-test.js',
+    'src/profit-margin-calc.js'
 ];
 
 // Config dev serving
@@ -26,78 +27,78 @@ const SERVE_ORIGIN = `http://localhost:${SERVE_PORT}`;
 
 // Create context
 const context = await esbuild.context({
-  bundle: true,
-  entryPoints: ENTRY_POINTS,
-  outdir: BUILD_DIRECTORY,
-  minify: PRODUCTION,
-  sourcemap: !PRODUCTION,
-  target: PRODUCTION ? 'es2019' : 'esnext',
-  inject: LIVE_RELOAD ? ['./bin/live-reload.js'] : undefined,
-  define: {
-    SERVE_ORIGIN: JSON.stringify(SERVE_ORIGIN),
-  },
-  external: ['jquery', 'gsap', 'gsap/ScrollTrigger', 'swiper'],
+    bundle: true,
+    entryPoints: ENTRY_POINTS,
+    outdir: BUILD_DIRECTORY,
+    minify: PRODUCTION,
+    sourcemap: !PRODUCTION,
+    target: PRODUCTION ? 'es2019' : 'esnext',
+    inject: LIVE_RELOAD ? ['./bin/live-reload.js'] : undefined,
+    define: {
+        SERVE_ORIGIN: JSON.stringify(SERVE_ORIGIN),
+    },
+    external: ['jquery', 'gsap', 'gsap/ScrollTrigger', 'swiper'],
 });
 
 // Build files in prod
 if (PRODUCTION) {
-  await context.rebuild();
-  context.dispose();
+    await context.rebuild();
+    context.dispose();
 }
 
 // Watch and serve files in dev
 else {
-  await context.watch();
-  await context
-    .serve({
-      servedir: BUILD_DIRECTORY,
-      port: SERVE_PORT,
-    })
-    .then(logServedFiles);
+    await context.watch();
+    await context
+        .serve({
+            servedir: BUILD_DIRECTORY,
+            port: SERVE_PORT,
+        })
+        .then(logServedFiles);
 }
 
 /**
  * Logs information about the files that are being served during local development.
  */
 function logServedFiles() {
-  /**
-   * Recursively gets all files in a directory.
-   * @param {string} dirPath
-   * @returns {string[]} An array of file paths.
-   */
-  const getFiles = (dirPath) => {
-    const files = readdirSync(dirPath, { withFileTypes: true }).map((dirent) => {
-      const path = join(dirPath, dirent.name);
-      return dirent.isDirectory() ? getFiles(path) : path;
-    });
+    /**
+     * Recursively gets all files in a directory.
+     * @param {string} dirPath
+     * @returns {string[]} An array of file paths.
+     */
+    const getFiles = (dirPath) => {
+        const files = readdirSync(dirPath, { withFileTypes: true }).map((dirent) => {
+            const path = join(dirPath, dirent.name);
+            return dirent.isDirectory() ? getFiles(path) : path;
+        });
 
-    return files.flat();
-  };
+        return files.flat();
+    };
 
-  const files = getFiles(BUILD_DIRECTORY);
+    const files = getFiles(BUILD_DIRECTORY);
 
-  const filesInfo = files
-    .map((file) => {
-      if (file.endsWith('.map')) return;
+    const filesInfo = files
+        .map((file) => {
+            if (file.endsWith('.map')) return;
 
-      // Normalize path and create file location
-      const paths = file.split(sep);
-      paths[0] = SERVE_ORIGIN;
+            // Normalize path and create file location
+            const paths = file.split(sep);
+            paths[0] = SERVE_ORIGIN;
 
-      const location = paths.join('/');
+            const location = paths.join('/');
 
-      // Create import suggestion
-      const tag = location.endsWith('.css')
-        ? `<link href="${location}" rel="stylesheet" type="text/css"/>`
-        : `<script defer src="${location}"></script>`;
+            // Create import suggestion
+            const tag = location.endsWith('.css')
+                ? `<link href="${location}" rel="stylesheet" type="text/css"/>`
+                : `<script defer src="${location}"></script>`;
 
-      return {
-        'File Location': location,
-        'Import Suggestion': tag,
-      };
-    })
-    .filter(Boolean);
+            return {
+                'File Location': location,
+                'Import Suggestion': tag,
+            };
+        })
+        .filter(Boolean);
 
-  // eslint-disable-next-line no-console
-  console.table(filesInfo);
+    // eslint-disable-next-line no-console
+    console.table(filesInfo);
 }
---
+++ src/profit-margin-calc.js
@@ -0,0 +1,160 @@
+// import tippy from 'tippy.js';
+// import 'tippy.js/dist/tippy.css';
+
+// import './scss/profit-margin-calc.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // does this page have the calculator?
+    let calc = document.querySelector('#profit-margin-calculator');
+
+    if (!calc) {
+        return;
+    }
+
+    // move form above the key takeaways
+    let mainWrap = document.querySelector('.blog-content_wrap-inner');
+    mainWrap.insertAdjacentElement('afterbegin', calc);
+
+    let formInputs = document.querySelectorAll('#profit-margin-calculator input');
+
+    // check each input as the user types.
+    Array.from(formInputs).forEach(input => {
+        input.addEventListener('input', (event) => {
+            formatCommas(event);
+        });
+    });
+
+
+    let calcBtn = document.querySelector('#btn--calculate');
+    calcBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // if the form is not valid, return early
+        let myForm = e.target.closest('form:invalid');
+        if (myForm) {
+            return;
+        }
+
+        // form is valid, proceed
+        // get inputs
+        let totalRevenue = parseFloat(document.querySelector('#total-revenue').value.replaceAll(',', '')) || 0;
+        let cogs = parseFloat(document.querySelector('#cogs').value.replaceAll(',', '')) || 0;
+        let operatingCosts = parseFloat(document.querySelector('#operating-costs').value.replaceAll(',', '')) || 0;
+
+        let grossProfit = roundOff(((totalRevenue - cogs) / totalRevenue) * 100, 2);
+        let netProfit = roundOff(((totalRevenue - operatingCosts) / totalRevenue) * 100, 2);
+
+        // render the output values
+        document.querySelector('#output--gross-profit').innerText = `${grossProfit}%`;
+        document.querySelector('#output--net-profit').innerText = `${netProfit}%`;
+
+
+        // determine industry comparison
+        let industry = document.querySelector('#restaurant-type').value;
+        let low, high;
+
+        switch (industry) {
+            case 'cafe':
+                low = 10;
+                high = 15;
+                break;
+
+            case 'casual-dining':
+                low = 3;
+                high = 9;
+                break;
+
+            case 'fast-casual':
+                low = 2;
+                high = 6;
+                break;
+
+            case 'fast-food':
+                low = 2;
+                high = 6;
+                break;
+
+            case 'fine-dining':
+                low = 10;
+                high = 15;
+                break;
+
+            case 'pizzeria':
+                low = 15;
+                high = 15;
+                break;
+
+            default:
+                low = 1;
+                high = 15;
+                break;
+        }
+
+        let conjunction = 'within';
+        let resultColor = 'off-black';
+
+        if (netProfit < low) {
+            conjunction = 'lower than';
+            resultColor = 'red';
+        } else if (netProfit > high) {
+            conjunction = 'higher than';
+            resultColor = 'green';
+        }
+
+        let message = `Your net profit margin is <strong style="color: var(--${resultColor})">${conjunction}</strong> the industry average of ${low}%-${high}%`;
+        document.querySelector('#output--message').innerHTML = message;
+        document.querySelector('#output--message').classList.remove('hide');
+    });
+
+
+    let resetBtn = document.querySelector('#btn--reset');
+    resetBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // clear input values
+        document.querySelector('#total-revenue').value = '';
+        document.querySelector('#cogs').value = '';
+        document.querySelector('#operating-costs').value = '';
+
+        // clear output values
+        document.querySelector('#output--gross-profit').innerText = `0%`;
+        document.querySelector('#output--net-profit').innerText = `0%`;
+
+        // scroll to the top of the calc
+        document.querySelector('#profit-margin-calculator').scrollIntoView({ behavior: "smooth", block: 'start' });
+    });
+
+    /*************************************************************************/
+    /**
+     * Format currency inputs as the user types
+     * @param {e} event 
+     */
+    function formatCommas(event) {
+        if (event.target.value.includes("-")) {
+            event.target.value = event.target.value.replace("-", '')
+        }
+        let temp = ((event.target.value).replace(/[^0-9-.]/g, ''));
+        temp = parseFloat(temp).toLocaleString("en-US");
+        event.target.value = temp.replace(' ', '');
+
+        if (event.target.value === "NaN") {
+            event.target.value = '';
+        }
+    }
+
+    function roundOff(num, places) {
+        const x = Math.pow(10, places);
+        return Math.round(num * x) / x;
+    }
+
+    // tooltip constructor
+    tippy('[data-tippy-content]', {
+        appendTo: 'parent',
+        arrow: false,
+
+        // useful for debugging and styling
+        // hideOnClick: false,
+        // trigger: 'click',
+    });
+})
